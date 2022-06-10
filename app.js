8e6b94bc055e07bb20aef46b4fe6ed03298985c1
Allison Anguiano <aanguiano@dev-10.com>
2022-06-10 14:19:24 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,72 @@
+const express = require('express')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
+const flash = require('connect-flash')
+const markdown = require('marked')
+const app = express()
+const sanitizeHTML = require('sanitize-html')
+
+let sessionOptions = session({
+    secret: "JavaScript is cool",
+    store: MongoStore.create({client: require('./db')}),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {maxAge: 1000 * 60 * 60 * 24, httpOnly: true}
+})
+
+app.use(sessionOptions)
+//maxAge is set to a day
+app.use(flash())
+
+//now have access to user property in any ejs template
+app.use(function(req, res, next) {
+    //make our markdown frunction available from within ejs templates
+    res.locals.filterUserHTML = function(content) {
+        return sanitizeHTML(markdown.parse(content), {allowedTags: ['p', 'br', 'ul', 'ol', 'li', 'strong', 'bold', 'i', 'em', 'h1', 'h2'], allowedAttributes: {}})
+    }
+
+    //make all error and success flash message avaialble from all templates
+    res.locals.errors = req.flash("errors")
+    res.locals.success = req.flash("success")
+    
+    //make current user id available on the req object
+    if (req.session.user) {req.visitorId = req.session.user._id} else {req.visitorId = 0}
+
+    // make user session available from within view template
+    res.locals.user = req.session.user
+    next()
+})
+
+const router = require('./router')
+
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+app.use(express.static('public'))
+app.set('views', 'views')
+app.set('view engine', 'ejs')
+
+app.use('/', router)
+
+const server = require('http').createServer(app)
+
+const io = require('socket.io')(server)
+
+//make express session data available from within context of socket
+io.use(function (socket, next) {
+    sessionOptions(socket.request, socket.request.res, next)
+  })
+
+io.on("connection", function (socket) {
+    if (socket.request.session.user) {
+      let user = socket.request.session.user
+  
+        socket.emit("welcome", { username: user.username, avatar: user.avatar })
+  
+        socket.on("chatMessageFromBrowser", function (data) {
+        socket.broadcast.emit("chatMessageFromServer", { message: sanitizeHTML(data.message, { allowedTags: [], allowedAttributes: {} }), username: user.username, avatar: user.avatar })
+      })
+    }
+  })
+
+module.exports = server
---
+++ models/Post.js
@@ -0,0 +1,192 @@
+const postsCollection = require('../db').db().collection('posts')
+const followsCollection = require('../db').db().collection('follows')   //not strictly mvc
+const ObjectID = require('mongodb').ObjectId
+const User = require('./User')
+const sanitizeHTML = require('sanitize-html')
+
+let Post = function(data, userid, requestedPostId) {
+    this.data = data
+    this.errors = []
+    this.userid = userid
+    this.requestedPostId = requestedPostId
+}
+
+Post.prototype.cleanUp = function() {
+    if (typeof(this.data.title) != "string") {this.data.title = ""}
+    if (typeof(this.data.body) != "string") {this.data.body = ""}
+
+    //get rid of any bogus properties
+    this.data = {
+        title: sanitizeHTML(this.data.title.trim(), {allowedTags: [], allowedAttributes: {}}),
+        body: sanitizeHTML(this.data.body.trim(), {allowedTags: [], allowedAttributes: {}}),
+        createdDate: new Date(),
+        author: ObjectID(this.userid)
+    }
+}
+
+Post.prototype.validate = function() {
+    if (this.data.title == "") {this.errors.push("You must provide a title")}
+    if (this.data.body == "") {this.errors.push("You must provide post content")}
+}
+
+Post.prototype.create = function() {
+    return new Promise((resolve, reject) => {
+        this.cleanUp()
+        this.validate()
+        if (!this.errors.length) {
+            // save post into data base
+            postsCollection.insertOne(this.data).then((info) => {
+                resolve(info.insertedId)
+            }).catch(() => {
+                this.errors.push("Please try again later")
+                reject(this.errors)
+            })
+        }
+        else {
+            reject(this.errors)
+        }
+    })
+}
+
+Post.prototype.update = function() {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let post = await Post.findSingleById(this.requestedPostId, this.userid)
+            if (post.isVisitorOwner) {
+                //actually update the db
+                let status = await this.actuallyUpdate()
+                resolve(status)
+            }
+            else {
+                reject()
+            }
+        } catch {
+            reject()
+        }
+    })
+}
+
+Post.prototype.actuallyUpdate = function() {
+    return new Promise(async (resolve, reject) => {
+        this.cleanUp()
+        this.validate()
+        if (!this.errors.length) {
+            await postsCollection.findOneAndUpdate({_id: new ObjectID(this.requestedPostId)}, {$set: {title: this.data.title, body: this.data.body}}) 
+            resolve("success")
+        }
+        else {
+            resolve("failure")
+        }
+    })
+}
+
+Post.reusablePostQuery = function(uniqueOperations, visitorId, finalOperations = []) {
+    return new Promise(async function(resolve, reject) {
+        let aggOperations = uniqueOperations.concat([
+            {$lookup: {from: "users", localField: "author", foreignField: "_id", as: "authorDocument"}},
+            {$project: {
+                title: 1,
+                body: 1,
+                createdDate: 1,
+                authorId: "$author",
+                author: {$arrayElemAt:["$authorDocument", 0]}
+            }}
+        ]).concat(finalOperations)
+
+        let posts = await postsCollection.aggregate(aggOperations).toArray()
+
+        // clean up author property in each post object
+        posts = posts.map(function(post) {
+            post.isVisitorOwner = post.authorId.equals(visitorId)
+            post.authorId = undefined
+
+            post.author = {
+                username: post.author.username,
+                avatar: new User(post.author, true).avatar
+            }
+
+            return post
+        })
+        resolve(posts)
+    })
+}
+
+Post.findSingleById = function(id, visitorId) {
+    return new Promise(async function(resolve, reject) {
+        if (typeof(id) != "string" || !ObjectID.isValid(id)) {
+            reject()
+            return
+        }
+        
+        let posts = await Post.reusablePostQuery([
+            {$match: {_id: new ObjectID(id)}}
+        ], visitorId)
+
+        if (posts.length) {
+            console.log(posts[0])
+            resolve(posts[0])
+        }
+        else {
+            reject()
+        }
+    })
+}
+
+Post.findByAuthorId = function(authourId) {
+    return Post.reusablePostQuery([
+        {$match: {author: authourId}},
+        {$sort: {createdDate: -1}}
+    ])
+}
+
+Post.delete = function(postIdToDelete, currentUserId) {
+    return new Promise(async (resolve, reject) => {
+        try{
+            let post = await Post.findSingleById(postIdToDelete, currentUserId)
+            if (post.isVisitorOwner) {
+                await postsCollection.deleteOne({_id: new ObjectID(postIdToDelete)})
+                resolve()
+            } else {
+                reject()
+            }
+        }
+        catch{
+            reject()
+        }
+    })
+}
+
+Post.search = function(searchTerm) {
+    return new Promise(async (resolve, reject) => {
+        if (typeof(searchTerm) == "string") {       //prevent malicious user from doing noSQL injection attack
+            let posts = await Post.reusablePostQuery([
+                {$match: {$text: {$search: searchTerm}}}           
+            ], undefined, [{$sort: {score: {$meta: "textScore"}}}])
+            resolve(posts)
+        } else {
+            reject()
+        }
+    })
+}
+
+Post.countPostsByAuthor = function(id) {
+    return new Promise(async (resolve, reject) => {
+        let postCount = await postsCollection.countDocuments({author: id})
+        resolve(postCount)
+    })
+}
+
+Post.getFeed = async function(id) {
+    // create an array of the user ids that the current user follows
+    let followedUsers = await followsCollection.find({authorId: new ObjectID(id)}).toArray()
+    followedUsers = followedUsers.map(function(followDoc) {
+        return followDoc.followedId
+    })
+    // look for posts where the author is in the above array of followed users
+    return Post.reusablePostQuery([
+        {$match: {author: {$in: followedUsers}}},    //find any post documnet where the author value is in the array of followed users
+        {$sort: {createdDate: -1}}
+    ])
+}
+
+module.exports = Post
